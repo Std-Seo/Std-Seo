ff433476e27f7616fa10aef63ad75565fdf5bd88
Std-Seo <101812506+Std-Seo@users.noreply.github.com>
2022-03-24 22:36:34 +0900

    Update and rename Math.java to Math_1.java

---
+++ Math_1.java
@@ -0,0 +1,41 @@
+public class math {
+	public static void main(String[] args) {
+		
+		// 2번 프린트 문제 6번
+		int x, y;
+		for(x = 0; x <= 5; x++) {
+			for(y = 0; y <= 5; y++) {
+				if(2 * x + y == 5) {
+					System.out.println("(" + x + "," + y + ")");
+				}
+			}
+		}
+		System.out.println("");
+		
+		// 2번 프린트 문제 7번
+		int a, b;
+		for(a = 0; a <= 10; a++) {
+			for(b = 0; b <= 10; b++) {
+				if(a + 4 * b == 10) {
+					System.out.println("(" + a + "," + b + ")");
+				}
+			}
+		}
+		System.out.println("");
+		
+		// 2번 프린트 문제 8번
+		int c, d;
+		for(c = 1; c <= 4; c++) {
+			for(d = 1; d <= 4; d++) {
+				if(c + d <= 4) {
+					System.out.println("(" + c + "," + d + ")");
+				}
+			}
+		}
+		System.out.println("");
+    
+
+      
+
+  
+
