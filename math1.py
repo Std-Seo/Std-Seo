cecfcfab980524e192f354965d2827997761d1f5
Std-Seo <101812506+Std-Seo@users.noreply.github.com>
2022-04-10 17:22:29 +0900

    Update math1.py

--- math1.py
+++ math1.py
@@ -1,6 +1,14 @@
+# 2x+y=5를 만족시키는 음이 아닌 정수의 순서쌍 구하기
 x = 0
 y = 0
 for x in range(0, 6):
     for y in range(0, 6):
         if(2 * x + y == 5):
+            print("(",x,",",y,")")
+print()
+
+# x+4y=10를 만족시키는 음이 아닌 정수의 순서쌍 구하기
+for x in range(0, 11):
+    for y in range(0, 11):
+        if(x + 4*y == 10):
             print("(",x,",",y,")")
